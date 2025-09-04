ecab0986df54c9725bdef50ae2330eac19e7d339
glennmichael123 <gtorregosa@gmail.com>
2025-09-04 23:23:19 +0800

    chore: wip

--- test/setup.ts
+++ test/setup.ts
@@ -3,11 +3,9 @@ import { config } from '../src/config'
 
 export async function setupDatabase() {
   try {
-    const result = await generateMigration('./examples/models', { dialect: config.dialect, full: true })
+    await generateMigration('./examples/models', { dialect: config.dialect, full: true })
 
-    if (result.sqlStatements.length > 0) {
-      await executeMigration()
-    }
+    await executeMigration()
   }
   catch (error) {
     console.error('Migration failed:', error)
