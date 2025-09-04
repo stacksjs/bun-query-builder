6d38297fb56370c6e774e4dfda0dc28d9a5cefbc
glennmichael123 <gtorregosa@gmail.com>
2025-09-04 21:22:44 +0800

    chore: wip

--- test/setup.ts
+++ test/setup.ts
@@ -1,17 +1,17 @@
-import { resetDatabase } from '../src/actions/migrate'
+import { executeMigration, generateMigration, resetDatabase } from '../src/actions/migrate'
 import { config } from '../src/config'
 
 export async function setupDatabase() {
   await resetDatabase('./examples/models', { dialect: config.dialect })
 
-  // try {
-  //   const result = await generateMigration('./examples/models', { dialect: config.dialect, full: true })
+  try {
+    const result = await generateMigration('./examples/models', { dialect: config.dialect, full: true })
 
-  //   if (result.sqlStatements.length > 0) {
-  //     await executeMigration()
-  //   }
-  // }
-  // catch (error) {
-  //   console.error('Migration failed:', error)
-  // }
+    if (result.sqlStatements.length > 0) {
+      await executeMigration()
+    }
+  }
+  catch (error) {
+    console.error('Migration failed:', error)
+  }
 }
