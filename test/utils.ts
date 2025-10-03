9209db0a9f6517ac0827d6213d4b7881147a7c6d
Chris <chrisbreuer93@gmail.com>
2025-10-03 13:19:46 -0700

    chore: wip

--- test/utils.ts
+++ test/utils.ts
@@ -1,14 +1,41 @@
 // Mock SQL function for testing that doesn't require database connections
-export function mockSql(strings: TemplateStringsArray, ...values: any[]) {
-  const query = strings.reduce((result, str, i) => {
-    return result + str + (values[i] !== undefined ? String(values[i]) : '')
-  }, '')
+export function mockSql(strings: TemplateStringsArray, ...values: any[]): any {
+  // Build query string by interpolating values
+  let query = ''
+  for (let i = 0; i < strings.length; i++) {
+    query += strings[i]
+    if (i < values.length) {
+      const val = values[i]
+      // If value is another query object, get its string representation
+      if (val && typeof val === 'object' && 'toString' in val) {
+        query += val.toString()
+      }
+      else if (val !== undefined) {
+        query += String(val)
+      }
+    }
+  }
 
-  return {
+  // Clean up extra whitespace
+  query = query.replace(/\s+/g, ' ').trim()
+
+  const result: any = {
     execute: () => Promise.resolve([]),
     toSQL: () => ({ sql: query, params: values }),
     toString: () => query,
+    // Make it chainable
+    then: undefined,
   }
+
+  // Support using as template literal
+  return new Proxy(result, {
+    get(target, prop) {
+      if (prop in target) {
+        return target[prop]
+      }
+      return undefined
+    },
+  })
 }
 
 // Mock query builder state for testing
